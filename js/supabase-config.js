c2e185a1d72a3f5ada290a4cf772d91067bc47ec
rafaelbeltramerb-hue <rafaelbeltrame.rb@gmail.com>
2026-09-11 14:57:42 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -5,6 +5,6 @@ window.PORTAL_CONFIG = {
 
 window.SUPABASE_CONFIG = {
   // Preencha somente com a Project URL e a chave pública do Supabase.
-  url: '',
-  anonKey: ''
+  url: 'https://xkijypdizvhrvstggltd.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhraWp5cGRpenZocnZzdGdnbHRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNDA5OTcsImV4cCI6MjEwNDYxNjk5N30.XgKZqVxHpyOqWBesOizyK4Pz0-wdpF_-JrxiE76kiFA'
 };
